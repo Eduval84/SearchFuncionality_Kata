187bfc9c8805e9d843b3f74f4be9fc0220e2f323
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-04 23:13:17 +0100

    Cracion del proyecto y la clase test

---
+++ TestSearchFuncionalityKata/TestSearchFuncionalityKata/SearchFuncionalitySpects.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace TestSearchFuncionalityKata
+{
+    public class SearchFuncionalitySpects
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
