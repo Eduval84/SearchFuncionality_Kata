641f57450a1a054ae28fca7206d5b09c8aa48fbf
evaderrama <evalderrama@domingoalonsogroup.com>
2022-04-05 00:11:16 +0100

    Refactor SearchFunctionality

--- TestSearchFuncionalityKata/TestSearchFuncionalityKata/SearchFunctionality.cs
+++ TestSearchFuncionalityKata/TestSearchFuncionalityKata/SearchFunctionality.cs
@@ -2,18 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace TestSearchFuncionalityKata;
 
 public class SearchFunctionality
 {
-    private IEnumerable<String> CityNames = new string[] {"Paris","Budapest","Skopje","Rotterdam","Valencia","Vancouver","Amsterdam","Vienna","Sydney","New York City","London","Bangkok", "Hong Kong", "Dubai", "Rome","Istanbul" };
+    private static string[] _cityNames = new[] {"Paris","Budapest","Skopje","Rotterdam","Valencia","Vancouver","Amsterdam","Vienna","Sydney","New York City","London","Bangkok", "Hong Kong", "Dubai", "Rome","Istanbul" };
 
     public static IEnumerable<string> Search(string input)
     {
         if (input.Length < 2 )
             return Enumerable.Empty<string>();
 
-        return new List<string>() {"Valencia", "Vancouver"};
+        return Array.FindAll<String>(_cityNames, citys => citys.Contains(input));
     }
+
 }
